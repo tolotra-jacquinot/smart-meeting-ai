997ab787118f3d5ad0013522f6ca73741ef61d16
Tolotra Jacquinot <tolotrajacquinot@gmail.com>
2026-08-18 11:56:58 +0300

    Fix audio validation workflow

--- meeting-backend/meetings/views.py
+++ meeting-backend/meetings/views.py
@@ -1,18 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-
-from .models import Meeting
-from .serializers import MeetingSerializer
-
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.exceptions import PermissionDenied, ValidationError
 
 from .models import Meeting, AudioFile
 from .serializers import MeetingSerializer, AudioFileSerializer
-
-from rest_framework.exceptions import PermissionDenied, ValidationError
 
 from .services.audio import (
     InvalidAudioError,
@@ -31,37 +23,7 @@ class MeetingListCreateView(generics.ListCreateAPIView):
         ).order_by('-created_at')
 
     def perform_create(self, serializer):
-        meeting = serializer.validated_data['meeting']
-
-        if meeting.owner != self.request.user:
-            raise PermissionDenied(
-                "You do not have permission to upload audio to this meeting."
-            )
-
-        uploaded_file = self.request.FILES.get('file')
-
-        audio = serializer.save(
-            original_name=uploaded_file.name if uploaded_file else ''
-        )
-
-        try:
-            validate_audio_with_ffprobe(audio.file.path)
-
-            duration = get_audio_duration(audio.file.path)
-
-        except InvalidAudioError as exc:
-            audio.file.delete(save=False)
-            audio.delete()
-
-            raise ValidationError({
-                "file": str(exc)
-            })
-
-        audio.duration = duration
-
-        audio.save(
-            update_fields=['duration']
-        )
+        serializer.save(owner=self.request.user)
 
 
 class MeetingDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -69,7 +31,10 @@ class MeetingDetailView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        return Meeting.objects.filter(owner=self.request.user)
+        return Meeting.objects.filter(
+            owner=self.request.user
+        )
+
 
 class AudioUploadView(generics.CreateAPIView):
     serializer_class = AudioFileSerializer
@@ -86,6 +51,21 @@ class AudioUploadView(generics.CreateAPIView):
 
         uploaded_file = self.request.FILES.get('file')
 
-        serializer.save(
+        audio = serializer.save(
             original_name=uploaded_file.name if uploaded_file else ''
         )
+
+        try:
+            validate_audio_with_ffprobe(audio.file.path)
+            duration = get_audio_duration(audio.file.path)
+
+        except InvalidAudioError as exc:
+            audio.file.delete(save=False)
+            audio.delete()
+
+            raise ValidationError({
+                "file": str(exc)
+            })
+
+        audio.duration = duration
+        audio.save(update_fields=['duration'])
